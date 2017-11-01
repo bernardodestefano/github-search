84b5e2fa766623bc60bcfc48fdc86329955d2ee3
Bernardo de Stefano <bernardo.destefano@gmail.com>
2017-11-02 00:02:32 +0100

    add createItem functionality and style

--- src/js/index.js
+++ src/js/index.js
@@ -9,12 +9,37 @@ let delayTimer;
 
 const emptyNode = (node) => node.innerHTML="";
 
+const createItem = (item) => {
+	const li = document.createElement("li");
+	const link = document.createElement("a");
+	const img = document.createElement("img");
+	const span = document.createElement("span");
+
+	li.classList.add('github-search__item');
+
+	img.setAttribute('src', item.avatar_url);
+	img.setAttribute('alt','user profile pic');
+	img.classList.add('github-search__item__img');
+
+	link.setAttribute('href', item.html_url);
+	link.setAttribute('target', '_blanck');
+	link.classList.add('github-search__item__link');
+
+	span.classList.add('github-search__item__name');
+	span.innerHTML = item.login;
+
+	li.appendChild(link);
+	link.appendChild(img);
+	link.appendChild(span);
+
+	return li;
+}
+
 const addResults = (results, node) => {
-	//only for testing
 	for(const user of results) {
-		console.log(user);
+		node.appendChild(createItem(user));
 	}
-	//TODO
+	node.firstElementChild.focus();
 };
 
 const searchRequest = (input) => {
